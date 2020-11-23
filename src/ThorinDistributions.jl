27a3afaaec8a505659e919663760f4b8abab4da0
Oskar Laverny <oskar.laverny@gmail.com>
2020-11-23 16:26:34 +0100

    including the file...

--- src/ThorinDistributions.jl
+++ src/ThorinDistributions.jl
@@ -3,5 +3,6 @@ module ThorinDistributions
 include("UnivariateGammaConvolution.jl")
 include("MultivariateGamma.jl")
 include("MultivariateGammaConvolution.jl")
+include("ConvolutionsAndproducts.jl")
 
 end
