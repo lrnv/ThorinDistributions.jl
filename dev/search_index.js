ce6eea359d13a82a6cf113b3b4506c8aeff4425e
Documenter.jl <documenter@juliadocs.github.io>
2020-11-23 14:53:21 +0000

    build based on bdfc3a8

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = ThorinDistributions","category":"page"},{"location":"#ThorinDistributions","page":"Home","title":"ThorinDistributions","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [ThorinDistributions]","category":"page"},{"location":"#ThorinDistributions.test-Tuple{Any}","page":"Home","title":"ThorinDistributions.test","text":"Docstring of the function test\n\n\n\n\n\n","category":"method"}]
+}
