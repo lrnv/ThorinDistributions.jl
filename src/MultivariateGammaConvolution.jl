fc54a6f19086208f5300cbf6d5ba95b7d46d6ff5
Oskar Laverny <oskar.laverny@gmail.com>
2021-12-27 18:09:50 +0100

    changing conditions of entry into the MGC constructor

--- src/MultivariateGammaConvolution.jl
+++ src/MultivariateGammaConvolution.jl
@@ -34,26 +34,24 @@ end
 function MultivariateGammaConvolution(α::AbstractVector{T1},θ::AbstractMatrix{T2}) where {T1 <: Real, T2<:Real}
 
 
-     @assert(length(α) == size(θ,1),"You must provide α with same length as the number of rows in θ")
-     # Propote eltypes :
-     T = Base.promote_eltype(α,θ,[1.0])
-     α = T.(α)
-     θ = T.(θ) # the size of theta is (n,d)
-
-     # Remove alphas that are non-positives or thetas that are all zero.
-     if any(α .< eps(T))
-         are_pos = α .> eps(T) | [all(θ[i,:] .> 0) for i in 1:size(θ,1)]
-         α = α[are_pos]
-         θ = θ[are_pos,:]
-     end
-     
-     if length(α) == 1
-         return MultivariateGamma(α,θ[:,1])
-     end
-     # sorting by alphas : 
-     θ =θ[sortperm(α),:]
-     sort!(α)
-     return MultivariateGammaConvolution(α,θ, 1)
+    @assert(length(α) == size(θ,1),"You must provide α with same length as the number of rows in θ")
+    # Propote eltypes :
+    T = Base.promote_eltype(α,θ,[1.0])
+    α = T.(α)
+    θ = T.(θ) # the size of theta is (n,d)
+    θ[θ .< eps(T)] .= 0
+    # Remove alphas that are non-positives or thetas that are all zero.
+    n = length(α)
+    are_pos = BitVector(undef,n)
+    for i in 1:n
+        are_pos[i] = α[i] > eps(T)
+        are_pos[i] &= any(θ[i,:] .> eps(T))
+    end
+    α = α[are_pos]
+    θ = θ[are_pos,:] 
+    θ = θ[sortperm(α),:]
+    sort!(α)
+    return MultivariateGammaConvolution(α,θ, 1)
 end
 MultivariateGammaConvolution(α::T1,θ::AbstractVector{T2}) where {T1<:Real, T2<:Real} = MultivariateGamma(promote(α,θ)...)
 MultivariateGammaConvolution(α::AbstractVector{T1},θ::AbstractVector{T2}) where {T1<:Real, T2<:Real} = UnivariateGammaConvolution(promote(α,θ)...)
